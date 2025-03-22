97429b445ae22c6275b959800b20e34c9aebcd65
Rifate nur shawn <33421686+Rifate-nur-shawn@users.noreply.github.com>
2025-03-23 02:15:40 +0600

    Practice

---
+++ react-dev/src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  function Mybutton() {
+    return <button>My button</button>;
+  }
+
+  return (
+    <>
+      <h1>Welcome to my app</h1>
+
+      <Mybutton />
+    </>
+  );
+}
+
+export default App;
